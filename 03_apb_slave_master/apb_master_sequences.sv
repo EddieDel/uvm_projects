738732fa111aef1b0dee35fbeef81d0d28d80d19
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-02-11 17:07:54 +0200

    Update apb_master_sequences.sv

--- 03_apb_slave_master/apb_master_sequences.sv
+++ 03_apb_slave_master/apb_master_sequences.sv
@@ -78,4 +78,25 @@ class apb_read_sequence extends uvm_sequence#(.REQ(apb_tx));
   endtask
 endclass
 
+
+// ========================================================================
+// Write then read sequence
+// ========================================================================
+
+class apb_write_then_read_sequence extends uvm_sequence#(.REQ(apb_tx));
+  `uvm_object_utils(apb_write_then_read_sequence)
+  
+  apb_tx item;
+  
+  function new (string name = "");
+    super.new(name);
+  endfunction
+  
+  virtual task body();    
+      `uvm_do_with(item, {direction == WRITE; paddr == 2'h00;});
+      `uvm_do_with(item, {direction == READ; paddr == 2'h00;});
+  endtask
+endclass
+
+
 `endif

49edaa60f1c1147e7b88f0d35c70e662b53d5e5d
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-02-23 20:30:02 +0200

    Create axi_write_agent_pkg.sv

---
+++ 04_AXI_project/axi_write_agent_pkg.sv
@@ -0,0 +1,20 @@
+`ifndef AXI_WRITE_AGENT_PKG_SV
+ `define AXI_WRITE_AGENT_PKG_SV
+
+`include "axi_pkg.sv"
+`include "axi_if.sv"
+`include "uvm_macros.svh"
+
+package axi_write_agent_pkg;
+import axi_pkg::*;
+import uvm_pkg::*;
+`include "axi_write_tx.sv";
+`include "axi_write_sequences.sv";
+`include "axi_write_sequencer.sv";
+`include "axi_write_driver.sv";
+`include "axi_write_agent.sv";
+
+
+endpackage
+
+`endif

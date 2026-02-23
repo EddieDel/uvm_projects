ca4033e87ef6841da7edf59bee571d9871debe39
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-02-23 20:30:29 +0200

    Create axi_write_agent.sv

---
+++ 04_AXI_project/axi_write_agent.sv
@@ -0,0 +1,41 @@
+`ifndef AXI_WRITE_AGENT_SV
+ `define AXI_WRITE_AGENT_SV
+
+class axi_write_agent extends uvm_agent;
+  `uvm_component_utils(axi_write_agent)
+  
+  //Variable declarations
+  virtual axi_if      vif;
+  axi_write_driver    write_driver;
+  axi_write_sequencer write_sequencer;
+  //monitor
+  //tlm_port  
+  
+  
+  function new (string name ="",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(virtual axi_if)::get(this,"","vif",vif)) begin
+      `uvm_fatal("Write Agent","Could not retrieve vif")
+    end
+    write_driver    = axi_write_driver::type_id::create("write_driver",this);
+    write_sequencer = axi_write_sequencer::type_id::create("write_sequencer",this);
+    
+    write_driver.vif = vif;
+    //create monitor
+    //create sequencer?
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    write_driver.seq_item_port.connect(write_sequencer.seq_item_export);
+    //connect monitor
+  endfunction
+  
+endclass
+
+`endif

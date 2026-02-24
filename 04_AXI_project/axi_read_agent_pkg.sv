374a3582af8abaaf2ed56b8b7991c8be581c628e
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-02-24 17:42:06 +0200

    Update axi_read_agent_pkg.sv

--- 04_AXI_project/axi_read_agent_pkg.sv
+++ 04_AXI_project/axi_read_agent_pkg.sv
@@ -1,10 +1,20 @@
 `ifndef AXI_READ_AGENT_PKG_SV
  `define AXI_READ_AGENT_PKG_SV
 
+`include "axi_pkg.sv"
+`include "axi_if.sv"
+`include "uvm_macros.svh"
 
 package axi_read_agent_pkg;
+import axi_pkg::*;
+import uvm_pkg::*;
 
-
+`include "axi_read_tx.sv";
+`include "axi_read_sequences.sv";
+`include "axi_read_sequencer.sv";
+`include "axi_read_driver.sv";
+`include "axi_read_monitor.sv";
+`include "axi_read_agent.sv";
 endpackage
 
 `endif

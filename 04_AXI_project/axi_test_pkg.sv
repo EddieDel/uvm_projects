03ed04a3e8dd9e7ecf7a6f94bc542d6ac0fc80d8
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-02-24 17:40:20 +0200

    Update axi_test_pkg.sv

--- 04_AXI_project/axi_test_pkg.sv
+++ 04_AXI_project/axi_test_pkg.sv
@@ -5,11 +5,12 @@
 `include "axi_if.sv"
 `include "axi_env_pkg.sv"
 `include "axi_write_agent_pkg.sv"
-
+`include "axi_read_agent_pkg.sv"
 package axi_test_pkg;
 import uvm_pkg::*;
 import axi_env_pkg::*;
 import axi_write_agent_pkg::*;
+import axi_read_agent_pkg::*;
 
  `include "axi_test_base.sv";
  `include "axi_tests.sv";

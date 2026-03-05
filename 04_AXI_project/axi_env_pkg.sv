e4930ab09c6826d370310a70fb6df3ea3c74c3e2
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-03-05 19:55:29 +0200

    Refactor imports and includes in axi_env_pkg.sv

--- 04_AXI_project/axi_env_pkg.sv
+++ 04_AXI_project/axi_env_pkg.sv
@@ -1,17 +1,23 @@
 `ifndef AXI_ENV_PKG_SV
  `define AXI_ENV_PKG_SV
-
+`define AXI_ENV_PKG_SV
 `include "uvm_macros.svh"
+`include "axi_pkg.sv"
 `include "axi_write_agent_pkg.sv"
 `include "axi_read_agent_pkg.sv"
 
 package axi_env_pkg;
-import uvm_pkg::*;
-import axi_write_agent_pkg::*;
-import axi_read_agent_pkg::*;
-
-`include "axi_env.sv";
-
+  import uvm_pkg::*;
+  import axi_pkg::*;
+  import axi_write_agent_pkg::*;
+  import axi_read_agent_pkg::*;
+  
+  `include "axi_write_sequences.sv" 
+  `include "axi_read_sequences.sv"
+  `include "axi_virtual_sequencer.sv"
+  `include "axi_virtual_sequences.sv"   
+  `include "axi_scoreboard.sv"
+  `include "axi_env.sv"
 endpackage
 
 `endif

dcf30d679eb484b10104630539a0a874251565d7
Eddie (Eneraldo) Delilaj <37222260+EddieDel@users.noreply.github.com>
2026-03-05 19:58:25 +0200

    Refactor axi_read_sequences class to axi_read_seq

--- 04_AXI_project/axi_read_sequences.sv
+++ 04_AXI_project/axi_read_sequences.sv
@@ -1,23 +1,30 @@
+// axi_read_sequences.sv
 `ifndef AXI_READ_SEQUENCES_SV
- `define AXI_READ_SEQUENCES_SV
+`define AXI_READ_SEQUENCES_SV
 
-class axi_read_sequences extends uvm_sequence#(.REQ(axi_read_tx));
-  `uvm_object_utils(axi_read_sequences)
+class axi_read_seq extends uvm_sequence#(axi_read_tx);
+  `uvm_object_utils(axi_read_seq)
   
-  axi_read_tx tx_item;
+  rand bit [ADDR_WIDTH-1:0] addr;
+  rand bit [7:0]  len;
+  rand bit [2:0]  size;
+  rand bit [1:0]  burst;
   
-  function new(string name ="");
+  function new(string name = "axi_read_seq");
     super.new(name);
   endfunction
   
   virtual task body();
-    axi_read_tx rsp;
-    repeat(30) begin
-      `uvm_do(tx_item);
-      get_response(rsp); // Get response for each item being sent
-    end
+    axi_read_tx r_tx = axi_read_tx::type_id::create("r_tx");
+    start_item(r_tx);
+    assert(r_tx.randomize() with {
+      araddr  == local::addr;
+      arlen   == local::len;
+      arsize  == local::size;
+      arburst == local::burst;
+    });
+    finish_item(r_tx);
   endtask
 endclass
-
 
 `endif
